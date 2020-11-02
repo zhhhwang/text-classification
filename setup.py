9ad7f9708efd46d8709d0ab9cccf092140ea6b16
zhhhwang <zhhhwang@umich.edu>
2020-11-01 19:09:50 -0500

    STUD-31 Adding project config

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='text_classification',
     packages=find_packages(),
     version='0.1.0',
-    description='kaggle fake news competition',
+    description='text classification tasks',
     author='zhhhwang',
     license='',
 )
