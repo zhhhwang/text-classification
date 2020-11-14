79646f94e4699caafdd9f28d936a0670c850db74
zhhhwang <zhhhwang@umich.edu>
2020-11-13 21:44:12 -0500

    STUD-32 Config testing

--- text_classification/config/constants.py
+++ text_classification/config/constants.py
@@ -1,4 +1,9 @@
+# Directory
 LOCAL_DATASETS_DIR = '/Users/zhhhwang/PycharmProjects/datasets/'
 
-FAKE_NEWS_DATA_DIR = '/Users/zhhhwang/PycharmProjects/datasets/kaggle_fake_news'
-AMAZON_REVIEW_DATA_DIR = '/Users/zhhhwang/PycharmProjects/datasets/amazon_reviews'
+# Datasets
+FAKE_NEWS_DATA_DIR = LOCAL_DATASETS_DIR + 'kaggle_fake_news'
+AMAZON_REVIEW_DATA_DIR = LOCAL_DATASETS_DIR + 'amazon_reviews'
+
+# Log file
+LOG_FILE = '/Users/zhhhwang/PycharmProjects/text-classification/reports/default.log'
