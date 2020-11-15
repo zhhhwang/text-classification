34ac2d0a30b30f592227d819fc99e4f7de527b4e
zhhhwang <zhhhwang@umich.edu>
2020-11-15 15:13:04 -0500

    STUD-32 Updating global constants.

--- text_classification/config/constants.py
+++ text_classification/config/constants.py
@@ -4,12 +4,8 @@ LOCAL_DATASETS_DIR = '/Users/zhhhwang/PycharmProjects/datasets/'
 # Datasets
 FAKE_NEWS_DATA_DIR = LOCAL_DATASETS_DIR + 'kaggle_fake_news'
 AMAZON_REVIEW_DATA_DIR = LOCAL_DATASETS_DIR + 'amazon_reviews'
-EMBEDDING_FILE_DIR = '../data/external/'
+EMBEDDING_FILE_DIR = '/Users/zhhhwang/PycharmProjects/text-classification/data/external/'
 
 # Glove Vec dataset url
 GLOVE_WIKI = 'http://nlp.stanford.edu/data/glove.6B.zip'
 GLOVE_TWITTER = 'http://nlp.stanford.edu/data/glove.twitter.27B.zip'
-
-
-# Log file
-LOG_FILE = '/Users/zhhhwang/PycharmProjects/text-classification/reports/default.log'
