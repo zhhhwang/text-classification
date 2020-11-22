6480556502130186fc890ef01ab73c099a3d90e3
zhhhwang <zhhhwang@umich.edu>
2020-11-22 10:12:46 -0500

    STUD-34 Adding utils for finding path and word freq count.

--- tests/test_model_utils.py
+++ tests/test_model_utils.py
@@ -1,7 +1,29 @@
 from text_classification.utils import model_utils
-import os
+import collections
+import pandas as pd
 
 
 def test_get_root_directory():
     test_path_name = model_utils.get_root_directory()
-    assert os.path.basename(test_path_name) == 'text-classification'
+
+    assert type(test_path_name) == str
+
+
+def test_get_word_frequency():
+    test_data = {'test_text': ['This', 'This', 'is', 'a', 'test']}
+    test_pd_df = pd.DataFrame(data=test_data)
+    test_word_freq = model_utils.get_word_frequency(test_pd_df.test_text)
+
+    assert type(test_word_freq) == collections.Counter
+    assert test_word_freq['This'] == 2
+
+
+def test_get_common_words():
+    test_data = {'test_text': ['this', 'this', 'this', 'is', 'is', 'a', 'test']}
+    test_pd_df = pd.DataFrame(data=test_data)
+    test_count = model_utils.get_word_frequency(test_pd_df.test_text)
+    expect_most_common = [('this', 3), ('is', 2)]
+    expect_least_common = [('a', 1), ('test', 1)]
+
+    assert all([i in model_utils.get_common_words(test_count, -2) for i in expect_least_common])
+    assert all([i in model_utils.get_common_words(test_count, 2) for i in expect_most_common])
--- text_classification/config/constants.py
+++ text_classification/config/constants.py
@@ -1,10 +1,12 @@
+from text_classification.utils import model_utils
+
 # Directory
-LOCAL_DATASETS_DIR = '/Users/zhhhwang/PycharmProjects/datasets/'
+LOCAL_DATASETS_DIR = model_utils.get_root_directory() + '/data'
 
 # Datasets
-FAKE_NEWS_DATA_DIR = LOCAL_DATASETS_DIR + 'kaggle_fake_news'
-AMAZON_REVIEW_DATA_DIR = LOCAL_DATASETS_DIR + 'amazon_reviews'
-EMBEDDING_FILE_DIR = '/Users/zhhhwang/PycharmProjects/text-classification/data/external/'
+FAKE_NEWS_DATA_DIR = LOCAL_DATASETS_DIR + '/interim/'
+AMAZON_REVIEW_DATA_DIR = LOCAL_DATASETS_DIR + '/interim/'
+EMBEDDING_FILE_DIR = LOCAL_DATASETS_DIR + '/external/'
 
 # Glove Vec dataset url
 GLOVE_WIKI = 'http://nlp.stanford.edu/data/glove.6B.zip'
