9daa128a9eac3652263e2bf88bf045b6b19fd910
zhhhwang <zhhhwang@umich.edu>
2020-11-22 19:39:17 -0500

    Another quick fix ... QAQ

--- text_classification/setup_python.sh
+++ text_classification/setup_python.sh
@@ -7,7 +7,7 @@ aws s3 cp $S3_DATA_LOCATION data/interim
 aws s3 cp $S3_GLOVE_DATA_LOCATION data/external
 
 head -100000 data/interim/Reviews.csv > data/interim/Reviews_small.csv
-unzip data/external/glove
+unzip data/external/glove -d data/external/
 
 # For setting up python
 pip3 install -r requirements.txt
