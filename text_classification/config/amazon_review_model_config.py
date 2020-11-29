ff16b6e92762a596971e8b64474cee897fdf0d61
zhhhwang <zhhhwang@umich.edu>
2020-11-28 22:57:22 -0500

    STUD-38 Fixing maximum feature issue.

--- text_classification/config/amazon_review_model_config.py
+++ text_classification/config/amazon_review_model_config.py
@@ -11,15 +11,28 @@ EPOCHS = 20
 EMBED_SIZE = 100
 LEARNING_RATE = 0.01
 
-# Call Back
-learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy',
-                                            patience=2,
-                                            verbose=1,
-                                            factor=0.5,
-                                            min_lr=0.000001)
+# Call Backs
+classification_learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy',
+                                                           patience=2,
+                                                           verbose=1,
+                                                           factor=0.5,
+                                                           min_lr=0.000001)
 
-early_stopping = EarlyStopping(monitor='val_accuracy',
-                               min_delta=0,
-                               patience=3,
-                               verbose=1,
-                               mode='auto')
+classification_early_stopping = EarlyStopping(monitor='val_accuracy',
+                                              min_delta=0,
+                                              patience=3,
+                                              verbose=1,
+                                              mode='auto')
+
+# Call Backs
+regression_learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy',
+                                                       patience=2,
+                                                       verbose=1,
+                                                       factor=0.5,
+                                                       min_lr=0.000001)
+
+regression_early_stopping = EarlyStopping(monitor='val_accuracy',
+                                          min_delta=0,
+                                          patience=3,
+                                          verbose=1,
+                                          mode='auto')
--- text_classification/models/amazon_review_classification.py
+++ text_classification/models/amazon_review_classification.py
@@ -10,8 +10,11 @@ from text_classification.config import amazon_review_model_config
 
 from sklearn.model_selection import train_test_split
 import logging
-import sys
 import numpy as np
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    handlers=[logging.FileHandler(constants.LOG_FILE, 'w+'), logging.StreamHandler()])
 
 
 def amazon_review_classification():
@@ -19,9 +22,6 @@ def amazon_review_classification():
     Main function that runs the kaggle fake news classification
     :return: None but could refer to logging info
     """
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(message)s',
-                        stream=sys.stdout)
 
     # Get the data
     df = get_dataset.get_amazon_review_data(constants.AMAZON_REVIEW_DATA_DIR)
@@ -35,7 +35,7 @@ def amazon_review_classification():
     logging.info("Text denoising completed.")
 
     # The following words have low frequency, please consider replacing them when necessary.
-    model_utils.get_common_words(model_utils.get_word_frequency(df.content), -10)
+    # model_utils.get_common_words(model_utils.get_word_frequency(df.content), -10)
     feature_count = model_utils.get_content_summary(df.content,
                                                     build_features.get_glove_embedding_coef(amazon_review_model_config.EMBEDDING_FILE),
                                                     least_common=50)
@@ -44,6 +44,24 @@ def amazon_review_classification():
     # Split training and testing
     x_train, x_test, y_train, y_test = train_test_split(df.content, df.Score, random_state=0)
 
+    # Modeling part
+    glove_embedding_lstm_classification(x_train, x_test, y_train, y_test, feature_count,
+                                        content_word_count)  # LSTM Classification
+    glove_embedding_lstm_regression(x_train, x_test, y_train, y_test, feature_count,
+                                    content_word_count)  # LSTM Regression
+
+
+def glove_embedding_lstm_classification(x_train, x_test, y_train, y_test, feature_count, content_word_count):
+    """
+    lstm model for classification into 1 - 5 levels
+    :param x_train:
+    :param x_test:
+    :param y_train:
+    :param y_test:
+    :param feature_count: How many words are seen in the corpus and will be used to create the maximum feature count
+    :param content_word_count: The 75 percentile of the content length * 1.2 and will be used for pad sequence
+    :return: lstm model
+    """
     # Text tokenizing and sequencing
     tokenizer = build_features.create_tokenizer_from_text(x_train, feature_count)
     x_train = build_features.create_text_sequence(x_train, tokenizer, content_word_count)
@@ -52,14 +70,52 @@ def amazon_review_classification():
     y_test = build_features.get_one_hot_encoding(y_test)
 
     # Getting glove embedding matrix
-    embedding_matrix = build_features.get_embedding_matrix(build_features.get_glove_embedding_coef(amazon_review_model_config.EMBEDDING_FILE),
-                                                           tokenizer,
-                                                           feature_count)
+    embedding_matrix = build_features.get_embedding_matrix(
+        build_features.get_glove_embedding_coef(amazon_review_model_config.EMBEDDING_FILE),
+        tokenizer)
 
     # Create and train the model
-    lstm_model = train_model.create_lstm_classification_model(embedding_matrix, len(tokenizer.word_index), content_word_count)
-    _ = train_model.train_model(lstm_model, x_train, y_train, x_test, y_test)
+    lstm_model = train_model.create_lstm_classification_model(embedding_matrix,
+                                                              len(tokenizer.word_index) + 1,
+                                                              content_word_count)
+    _ = train_model.train_model(lstm_model, x_train, y_train, x_test, y_test, classification=True)
+    print("Accuracy of the model on Training Data is - ", lstm_model.evaluate(x_train, y_train)[1] * 100, "%")
+    print("Accuracy of the model on Testing Data is - ", lstm_model.evaluate(x_test, y_test)[1] * 100, "%")
     _ = predict_model.get_confusion_matrix(lstm_model.predict_classes(x_test), np.argmax(y_test, axis=1))
+
+    return lstm_model
+
+
+def glove_embedding_lstm_regression(x_train, x_test, y_train, y_test, feature_count, content_word_count):
+    """
+
+    :param x_train:
+    :param x_test:
+    :param y_train:
+    :param y_test:
+    :param feature_count:
+    :param content_word_count:
+    :return:
+    """
+    # Text tokenizing and sequencing
+    tokenizer = build_features.create_tokenizer_from_text(x_train, feature_count)
+    x_train = build_features.create_text_sequence(x_train, tokenizer, content_word_count)
+    x_test = build_features.create_text_sequence(x_test, tokenizer, content_word_count)
+
+    # Getting glove embedding matrix
+    embedding_matrix = build_features.get_embedding_matrix(
+        build_features.get_glove_embedding_coef(amazon_review_model_config.EMBEDDING_FILE),
+        tokenizer)
+
+    # Create and train the model
+    lstm_model = train_model.create_lstm_regression_model(embedding_matrix,
+                                                          len(tokenizer.word_index) + 1,
+                                                          content_word_count)
+    _ = train_model.train_model(lstm_model, x_train, y_train, x_test, y_test, classification=False)
+    print("Accuracy of the model on Training Data is - ", lstm_model.evaluate(x_train, y_train))
+    print("Accuracy of the model on Testing Data is - ", lstm_model.evaluate(x_test, y_test))
+
+    return lstm_model
 
 
 if __name__ == '__main__':
